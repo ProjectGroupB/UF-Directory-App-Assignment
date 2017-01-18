4277ea3b25dead01503618d7a3bc2278e02a03be
Nuchaz <nuchaz.ipod@gmail.com>
2017-01-18 16:07:09 -0500

    Uploading Stevens code
    Added and error check on the connection and closed the connection, the rest is Stevens work. I also removed and commented out some extra console.log stuff.

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,48 +1,50 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
+/*
+ Import modules/files you may need to correctly run the script.
+ Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.schema,
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config'),
-    listArray;
+    assert = require('assert'),
+    json = require('./listings.json');
+
 
 /* Connect to your database */
 mongoose.connect(config.db.uri);
 var database = mongoose.connection;
-database.on('error', console.error.bind(console, 'error connecting:'));
-
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+database.on('error', console.error.bind(console, 'error connecting:')); // added a connection error check
+/*
+ Instantiate a mongoose model for each listing object in the JSON file,
+ and then save it to your Mongo database
  */
-database.once('open', function() {
-    console.log("connection established");
-    fs.readFile("listings.json", "utf8", function (err, data) {
-        console.log("reading json file");
-        listArray = JSON.parse(data); // TODO this doesn't work. Everything I read says this should turn the json string data into an array. But all I can get is undefined
-        for (var i = 0; i < listArray.length; i++){
-            var jsonEntry = listArray[i];
-            var entry  = new Listing();
-            entry.name = jsonEntry.name;
-            entry.code = jsonEntry.code;
-            entry.address = jsonEntry.address;
-            entry.latitude = jsonEntry.latitude;
-            entry.longitude = jsonEntry.longitude;
-            // TODO save the entry to the database
-        }
-    });
-    database.close();
+var listing = mongoose.model('Listing', Listing.listingSchema);
+var data;
+fs.readFile('./listings.json', 'utf8', function read(err, content) {
+    if(err) {
+        throw err;
+        console.log('error reading json file');
+    }
+    console.log('data read');
+    data = JSON.parse(content);
+    //console.log(data.entries[0]);
+
+    //console.log(data.entries.length);
+    for(var i = 0; i < data.entries.length; i++) {
+        //console.log(i);
+        var entry = new listing(data.entries[i]).save(function(err) {
+            if(err) console.log('file created');
+        });
+    }
+    console.log("json data moved successfully moved to database");
+    database.close(); //closes the connection when done
 });
+/*
 
-
-
-
-
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+ */
+/*
+ Once you've written + run the script, check out your MongoLab database to ensure that
+ it saved everything correctly.
  */
